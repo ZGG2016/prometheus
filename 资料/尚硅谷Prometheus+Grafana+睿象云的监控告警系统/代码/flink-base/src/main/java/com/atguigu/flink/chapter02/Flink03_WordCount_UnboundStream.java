fc898e32241237d54df3e3174d7f210c52f05912
zhang-guoguang <sdut2012@163.com>
2023-09-13 22:07:43 +0800

    add first

---
+++ 资料/尚硅谷Prometheus+Grafana+睿象云的监控告警系统/代码/flink-base/src/main/java/com/atguigu/flink/chapter02/Flink03_WordCount_UnboundStream.java
@@ -0,0 +1,40 @@
+package com.atguigu.flink.chapter02;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+/**
+ * Author: Felix
+ * Desc: 入门案例  无界 流处理
+ */
+public class Flink03_WordCount_UnboundStream {
+    public static void main(String[] args) throws Exception {
+        //创建流处理环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        //StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+        //从指定端口读取数据
+        DataStreamSource<String> dataStreamSource = env.socketTextStream("bigdata101", 9999);
+
+        //对数据进行结构转换 分组  求和
+        SingleOutputStreamOperator<Tuple2<String, Long>> resDS = dataStreamSource.flatMap(
+            new FlatMapFunction<String, Tuple2<String, Long>>() {
+                @Override
+                public void flatMap(String line, Collector<Tuple2<String, Long>> out) throws Exception {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        out.collect(Tuple2.of(word, 1L));
+                    }
+                }
+            }
+        )
+            .keyBy(t -> t.f0)
+            .sum(1);
+
+        resDS.print();
+        env.execute();
+    }
+}
